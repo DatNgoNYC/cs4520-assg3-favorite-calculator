0ba19615efbdb6d1ab767b7c702320ce1f96f4ad
Dat Ngo <datngonyc@gmail.com>
2024-02-24 15:23:06 -0500

    Complete the xml UI

--- app/src/main/java/com/cs4520/assignment3/MvpCalculator/ViewFragment.kt
+++ app/src/main/java/com/cs4520/assignment3/MvpCalculator/ViewFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.cs4520.assignment3.R
 
-class View : Fragment() {
+class ViewFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
