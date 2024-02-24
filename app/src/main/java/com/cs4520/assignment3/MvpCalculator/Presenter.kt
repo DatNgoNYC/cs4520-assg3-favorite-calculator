3b8f3abc2f14c2693f7a353a31cc297a848c8c60
Dat Ngo <datngonyc@gmail.com>
2024-02-24 17:39:00 -0500

    Complete Presenter abstraction

--- app/src/main/java/com/cs4520/assignment3/MvpCalculator/Presenter.kt
+++ app/src/main/java/com/cs4520/assignment3/MvpCalculator/Presenter.kt
@@ -1,4 +1,41 @@
 package com.cs4520.assignment3.MvpCalculator
 
-class Presenter {
+class Presenter(private val view: Contract.View, private val model: Model) : Contract.Presenter {
+    override fun onAddClicked(a: String, b: String) {
+        try {
+            val sum = model.add(a.toDouble(), b.toDouble())
+            view.showResult(sum.toString())
+            view.clearInputs()
+        } catch (exception: Exception) {
+            view.showError(exception.toString())
+        }
+    }
+
+    override fun onSubtractClicked(a: String, b: String) {
+        try {
+            val difference = model.subtract(a.toDouble(), b.toDouble())
+            view.showResult(difference.toString())
+            view.clearInputs()
+        } catch (exception: Exception) {
+            view.showError(exception.toString())
+        }
+    }
+
+    override fun onMultiplyClicked(a: String, b: String) {
+        try {
+            val product = model.multiply(a.toDouble(), b.toDouble())
+            view.showResult(product.toString())
+            view.clearInputs()
+        } catch (exception: Exception) {
+            view.showError(exception.toString())
+        }    }
+
+    override fun onDivideClicked(a: String, b: String) {
+        try {
+            val quotient = model.divide(a.toDouble(), b.toDouble())
+            view.showResult(quotient.toString())
+            view.clearInputs()
+        } catch (exception: Exception) {
+            view.showError(exception.toString())
+        }    }
 }
