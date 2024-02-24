8f1ff701ad4f6e99b9e176eb5149b0f960f5937e
Dat Ngo <datngonyc@gmail.com>
2024-02-24 14:30:26 -0500

    Create contract between the view and presenter.

--- app/src/main/java/com/cs4520/assignment3/MvpCalculator/Contract.kt
+++ app/src/main/java/com/cs4520/assignment3/MvpCalculator/Contract.kt
@@ -1,4 +1,16 @@
 package com.cs4520.assignment3.MvpCalculator
 
 interface Contract {
+    interface View {
+        fun showResult(result: String)
+        fun showError(message: String)
+        fun clearInputs()
+    }
+
+    interface Presenter {
+        fun onAddClicked(a: Double, b: Double)
+        fun onSubtractClicked(a: Double, b: Double)
+        fun onMultiplyClicked(a: Double, b: Double)
+        fun onDivideClicked(a: Double, b: Double)
+    }
 }
