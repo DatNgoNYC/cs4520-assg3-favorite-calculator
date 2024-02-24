d1ab91aa9a6d8b619bc7a9574da28917dea25dd3
Dat Ngo <datngonyc@gmail.com>
2024-02-24 15:22:43 -0500

    Create the mvp calculator model

--- app/src/main/java/com/cs4520/assignment3/MvpCalculator/Model.kt
+++ app/src/main/java/com/cs4520/assignment3/MvpCalculator/Model.kt
@@ -1,4 +1,10 @@
 package com.cs4520.assignment3.MvpCalculator
 
 class Model {
+    fun add(a: Double, b: Double) = a + b
+    fun subtract(a: Double, b: Double) = a - b
+    fun multiply(a: Double, b: Double) = a * b
+    fun divide(a: Double, b: Double) {
+        if (b == 0.0) throw ArithmeticException("Cannot divide by zero")
+    }
 }
