02bf7ced83308318bccf77c834e83aac7d8a11bd
Dat Ngo <datngonyc@gmail.com>
2024-02-26 17:10:16 -0500

    Added toast error message field in VIEWMODEL and use it in VIEW

--- app/src/main/java/com/cs4520/assignment3/MvvmCalculator/MvvmCalcFragment.kt
+++ app/src/main/java/com/cs4520/assignment3/MvvmCalculator/MvvmCalcFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -35,11 +36,38 @@ class MvvmCalcFragment : Fragment() {
             binding.resultField.visibility = View.VISIBLE
         })
 
+        viewModel.errorMessage.observe(viewLifecycleOwner, Observer { errorMessage ->
+            Toast.makeText(context, errorMessage, Toast.LENGTH_SHORT).show()
+        })
+
         // Set up event listeners for each operation button
-        binding.buttonAdd.setOnClickListener{
+        binding.buttonAdd.setOnClickListener {
             viewModel.add(
                 binding.input1.text.toString().toDouble(),
-                binding.input2.text.toString().toDouble())}
+                binding.input2.text.toString().toDouble()
+            )
+        }
+
+        binding.buttonSubtract.setOnClickListener {
+            viewModel.subtract(
+                binding.input1.text.toString().toDouble(),
+                binding.input2.text.toString().toDouble()
+            )
+        }
+
+        binding.buttonMultiply.setOnClickListener {
+            viewModel.multiply(
+                binding.input1.text.toString().toDouble(),
+                binding.input2.text.toString().toDouble()
+            )
+        }
+
+        binding.buttonDivide.setOnClickListener {
+            viewModel.divide(
+                binding.input1.text.toString().toDouble(),
+                binding.input2.text.toString().toDouble()
+            )
+        }
 
     }
 
--- app/src/main/java/com/cs4520/assignment3/MvvmCalculator/MvvmCalcViewModel.kt
+++ app/src/main/java/com/cs4520/assignment3/MvvmCalculator/MvvmCalcViewModel.kt
@@ -8,12 +8,41 @@ import com.cs4520.assignment3.MvpCalculator.Model
 class MvvmCalcViewModel : ViewModel() {
     private var _result = MutableLiveData<Double>()
     val result: LiveData<Double> get() = _result
+    private var _errorMessage = MutableLiveData<String>()
+    val errorMessage: LiveData<String> get() = _errorMessage
 
     // We will use the calculator model from MVP
     private val calculator = Model()
 
     fun add(a: Double, b: Double) {
-        _result.value = calculator.add(a, b);
+        try {
+            _result.value = calculator.add(a, b);
+        } catch (exception: Exception) {
+            _errorMessage.value = exception.toString()
+        }
     }
 
+    fun subtract(a: Double, b: Double) {
+        try {
+            _result.value = calculator.subtract(a, b);
+        } catch (exception: Exception) {
+            _errorMessage.value = exception.toString()
+        }
+    }
+
+    fun multiply(a: Double, b: Double) {
+        try {
+            _result.value = calculator.multiply(a, b);
+        } catch (exception: Exception) {
+            _errorMessage.value = exception.toString()
+        }
+    }
+
+    fun divide(a: Double, b: Double) {
+        try {
+            _result.value = calculator.divide(a, b);
+        } catch (exception: Exception) {
+            _errorMessage.value = exception.toString()
+        }
+    }
 }
