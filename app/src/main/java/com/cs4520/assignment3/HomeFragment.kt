600a6b9e4826e13a8f3833ef74bac080a2f3acb6
Dat Ngo <datngonyc@gmail.com>
2024-02-26 22:57:31 -0500

    REFACTORED homefragment from findbyid to viewbinding

--- app/src/main/java/com/cs4520/assignment3/HomeFragment.kt
+++ app/src/main/java/com/cs4520/assignment3/HomeFragment.kt
@@ -7,25 +7,32 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import com.cs4520.assignment3.databinding.HomepageFragmentBinding
+import com.cs4520.assignment3.databinding.MvpCalculatorFragmentBinding
 
 class HomeFragment : Fragment() {
+
+    private var _binding: HomepageFragmentBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.homepage_fragment, container, false)
+        _binding = HomepageFragmentBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.mvp_button).setOnClickListener {
+        binding.mvpButton.setOnClickListener {
             findNavController().navigate(R.id.action_homeFragment_to_mvpCalculatorFragment)
         }
 
-        view.findViewById<Button>(R.id.mvvm_button).setOnClickListener( {
+        binding.mvvmButton.setOnClickListener {
             findNavController().navigate(R.id.action_homeFragment_to_mvvmCalculatorFragment)
-        })
+        }
     }
 }
