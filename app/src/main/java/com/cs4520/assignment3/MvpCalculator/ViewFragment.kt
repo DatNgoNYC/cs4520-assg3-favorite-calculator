27e6c4b7a6ec7da8252f34ea8ff16339cf23791e
Dat Ngo <datngonyc@gmail.com>
2024-02-26 22:52:46 -0500

    REFACTOR from findbyid to viewbinding.

--- app/src/main/java/com/cs4520/assignment3/MvpCalculator/ViewFragment.kt
+++ app/src/main/java/com/cs4520/assignment3/MvpCalculator/ViewFragment.kt
@@ -11,64 +11,60 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.cs4520.assignment3.R
+import com.cs4520.assignment3.databinding.MvpCalculatorFragmentBinding
+import com.cs4520.assignment3.databinding.MvvmCalculatorFragmentBinding
 
 class ViewFragment : Fragment(), Contract.View {
 
-    private lateinit var input1: EditText
-    private lateinit var input2: EditText
-    private lateinit var resultField: TextView
+    private var _binding: MvpCalculatorFragmentBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstaceState: Bundle?
     ): View? {
-        // We're inflating the layout (xml file) for this fragment into a "View" object.
-        val view = inflater.inflate(R.layout.mvp_calculator_fragment, container, false);
+        _binding = MvpCalculatorFragmentBinding.inflate(inflater, container, false)
 
         // Initialize the presenter that we'll use later when creating eventListeners
         val model = Model()
         val presenter = Presenter(this, model)
 
-        // Find the EditTexts and TextView to use as "View" objects later when we need to change UI
-        input1 = view.findViewById(R.id.input_1)
-        input2 = view.findViewById(R.id.input_2)
-        resultField = view.findViewById(R.id.resultField)
 
         // Setting up the buttons' click listeners
-        view.findViewById<Button>(R.id.buttonAdd).setOnClickListener {
+        binding.buttonAdd.setOnClickListener {
             presenter.onAddClicked(
-                input1.text.toString(),
-                input2.text.toString()
+                binding.input1.text.toString(),
+                binding.input2.text.toString()
             )
         }
 
-        view.findViewById<Button>(R.id.buttonSubtract).setOnClickListener {
+        binding.buttonSubtract.setOnClickListener {
             presenter.onSubtractClicked(
-                input1.text.toString(),
-                input2.text.toString()
+                binding.input1.text.toString(),
+                binding.input2.text.toString()
             )
         }
 
-        view.findViewById<Button>(R.id.buttonMultiply).setOnClickListener {
+        binding.buttonMultiply.setOnClickListener {
             presenter.onMultiplyClicked(
-                input1.text.toString(),
-                input2.text.toString()
+                binding.input1.text.toString(),
+                binding.input2.text.toString()
             )
         }
 
-        view.findViewById<Button>(R.id.buttonDivide).setOnClickListener {
+        binding.buttonDivide.setOnClickListener {
             presenter.onDivideClicked(
-                input1.text.toString(),
-                input2.text.toString()
+                binding.input1.text.toString(),
+                binding.input2.text.toString()
             )
         }
 
-        return view
+        return binding.root
     }
 
     override fun showResult(result: String) {
-        resultField.visibility = View.VISIBLE
-        resultField.text = result
+        binding.resultField.visibility = View.VISIBLE
+        binding.resultField.text = result
     }
 
     override fun showError(message: String) {
@@ -76,8 +72,8 @@ class ViewFragment : Fragment(), Contract.View {
     }
 
     override fun clearInputs() {
-        input1.text.clear()
-        input2.text.clear()
+        binding.input1.text.clear()
+        binding.input2.text.clear()
     }
 
 }
